e994342df6d6b04720ba065d54a8ad928bc201c6
Carley Tripp <carley.tripp@outlook.com>
2018-08-26 03:40:26 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+<<<<<<< HEAD
 end
+=======
+end
+>>>>>>> c81ba5468f7153318feedcd09cfb620a8b8d0346
